73ee0f906bf6ba2791285b139a12155d4049ba54
María HMR <maria.hurtado.m.r@gmail.com>
2019-07-17 11:19:20 +0200

    Selfing

--- SEEDSET_SELFING.R
+++ SEEDSET_SELFING.R
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 # seed set --> selfing 
 library(tidyverse)
+#SF_2019 es el archivo de selfing de pupa, lema y chfu
 boxplot(SF_2019$VIABLES_SEEDS~ SF_2019$SELFING, xlab= ('selfing'), col= c(123, 456,234), ylab = ('viable seeds'),main = ('Viable seeds vs selfing: LEMA, PUPA Y CHFU'))
 t.test(SF_2019$VIABLES_SEEDS~SF_2019$SELFING, data= SF_2019)
 aov(VIABLES_SEEDS ~ SELFING, data = SF_2019)
@@ -18,48 +18,42 @@ SF <- ggplot(SF_2019, aes(x= SELFING, y = VIABLES_SEEDS))+
     ylab('SEMILLAS VIABLES')+
     ggtitle ('SEMILLAS VIABLES')+
 NULL
-SF
-#ANOVAS
-m <- lm(VIABLES_SEEDS ~ SELFING, data = SF_CHFU)
-summary(m)
-plot(m)
+SF #hacerlo con cajas estaría mejor
 
-aov(VIABLES_SEEDS ~ SELFING, data = SF_LEMA)
 
 #BUscar soluciones, no me sale este barplot
-PLANT= factor(SF_2019$PLANT, levels = c ("CHFU","LEMA"))
-barplot(table(SF_2029$VIABLES_SEEDS, PLANT), beside = TRUE, legend.text = T, col= c(123, 345))
+#PLANT<-factor(SF_2019$PLANT, levels = c ("CHFU","LEMA"))
+#barplot(table(SF_2019$VIABLES_SEEDS, PLANT), beside = T, legend.text = T, col= c(123, 345))
 
 
-#chfu
+#chfu----
 SF_CHFU <- subset(SF_2019, PLANT== 'CHFU')
 boxplot(SF_CHFU$VIABLES_SEEDS~SF_CHFU$SELFING, xlab= ('selfing'), ylab = ('viable seeds'), main = ('Viable seeds vs selfing: CHFU'))
-CHFU <- t.test(SF_CHFU$VIABLES_SEEDS~SF_CHFU$SELFING, data= SF_CHFU)
 m <- lm(VIABLES_SEEDS ~ SELFING, data = SF_CHFU)
 summary(m)
+aov(VIABLES_SEEDS ~ SELFING, data = SF_CHFU)
 
-
-#LEMA
+#LEMA ----
 SF_LEMA <- subset(SF_2019, PLANT== 'LEMA')
 boxplot(SF_LEMA$VIABLES_SEEDS~SF_LEMA$SELFING, xlab= ('selfing'), ylab = ('viable seeds'), main = ('Viable seeds vs selfing: LEMA'))
-LEMA <- t.test(SF_LEMA$VIABLES_SEEDS~SF_LEMA$SELFING, data= SF_LEMA)
 n <- lm(VIABLES_SEEDS ~ SELFING, data = SF_LEMA)
 summary(n)
+aov(VIABLES_SEEDS ~ SELFING, data = SF_LEMA)
 
-#pupa
+#pupa ----
 SF_PUPA <- subset(SF_2019, PLANT== 'PUPA')
 boxplot(SF_PUPA$VIABLES_SEEDS~SF_PUPA$SELFING, xlab= ('selfing'), ylab = ('viable seeds'), main = ('Viable seeds vs selfing: PUPA'))
-LEMA <- t.test(SF_PUPA$VIABLES_SEEDS~SF_PUPA$SELFING, data= SF_PUPA)
+aov(VIABLES_SEEDS ~ SELFING, data = SF_PUPA)
 O <- lm(VIABLES_SEEDS ~ SELFING, data = SF_PUPA)
 summary(O)
 plot(O)
 
-#según los analisis anteriores se podría decir que a CHFU los polinizadores le profucen un mayor fitness 
+#según los analisis anteriores se podría decir que a CHFU los polinizadores le producen un mayor fitness 
     #que a LEMA y PUPA, que no dependen tanto de los polinizadores. LEMa tiene altas tasas de autofecundación
-=======
+################
 # seed set --> selfing 
 library(tidyverse)
-boxplot(SF_2019$VIABLES_SEEDS~ SF_2019$SELFING, xlab= ('selfing'), col= c(123, 456), ylab = ('viable seeds'),main = ('Viable seeds vs selfing: LEMA Y CHFU'))
+boxplot(SF_2019$VIABLES_SEEDS~ SF_2019$SELFING, xlab= ('selfing'), col= c(123, 456), ylab = ('viable seeds'),main = ('Viable seeds vs selfing: LEMA, PUPA y CHFU'))
 t.test(SF_2019$VIABLES_SEEDS~SF_2019$SELFING, data= SF_2019)
 SF_2019$SELFING <- as.factor(SF_2019$SELFING)
 str(SF_2019)
@@ -72,29 +66,12 @@ SF <- ggplot(SF_2019, aes(x= SELFING, y = VIABLES_SEEDS))+
     ylab('SEMILLAS VIABLES')+
     ggtitle ('SEMILLAS VIABLES')+
 NULL
-SF
-#ANOVAS
-m <- lm(VIABLES_SEEDS ~ SELFING, data = SF_CHFU)
-summary(m)
-plot(m)
-aov(VIABLES_SEEDS ~ SELFING, data = SF_LEMA)
+SF #estaría mejor si consigo hacerlo con cajas, 2 por especie (1 de no viables y otra de viables)
 
 #BUscar soluciones, no me sale este barplot
-PLANT= factor(SF_2019$PLANT, levels = c ("CHFU","LEMA"))
-barplot(table(SF_2029$VIABLES_SEEDS, PLANT), beside = TRUE, legend.text = T, col= c(123, 345))
+#PLANT= factor(SF_2019$PLANT, levels = c ("CHFU","LEMA"))
+#barplot(table(SF_2029$VIABLES_SEEDS, PLANT), beside = TRUE, legend.text = T, col= c(123, 345))
 
 
 
 
-#chfu
-SF_CHFU <- subset(SF_2019, PLANT== 'CHFU')
-boxplot(SF_CHFU$VIABLES_SEEDS~SF_CHFU$SELFING, xlab= ('selfing'), ylab = ('viable seeds'), main = ('Viable seeds vs selfing: CHFU'))
-CHFU <- t.test(SF_CHFU$VIABLES_SEEDS~SF_CHFU$SELFING, data= SF_CHFU)
-#LEMA
-SF_LEMA <- subset(SF_2019, PLANT== 'LEMA')
-boxplot(SF_LEMA$VIABLES_SEEDS~SF_LEMA$SELFING, xlab= ('selfing'), ylab = ('viable seeds'), main = ('Viable seeds vs selfing: LEMA'))
-LEMA <- t.test(SF_LEMA$VIABLES_SEEDS~SF_LEMA$SELFING, data= SF_LEMA)
-
-
-
->>>>>>> b226452df8fd566327622aa60bf78d24bb286053
