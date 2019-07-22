10f3d17585b4b10cef7b7f2c5d52ce5125aee042
Nacho <nacho@air>
2019-07-22 09:42:26 +0200

    Looks good, but I need the data to look at it.

--- SEEDSET_SELFING.R
+++ SEEDSET_SELFING.R
@@ -1,16 +1,29 @@
 # seed set --> selfing 
+
+#load libraries:
 library(tidyverse)
+
+#load data:
+#Please load it here, or is hard to guess which file you are using.
+# In fact, there is no data in the repo!! Add it as csv's.
+# It means I am reading over the code, but without actually beeing able to run it.
+
 #SF_2019 es el archivo de selfing de pupa, lema y chfu
 boxplot(SF_2019$VIABLES_SEEDS~ SF_2019$SELFING, xlab= ('selfing'), col= c(123, 456,234), ylab = ('viable seeds'),main = ('Viable seeds vs selfing: LEMA, PUPA Y CHFU'))
 t.test(SF_2019$VIABLES_SEEDS~SF_2019$SELFING, data= SF_2019)
 aov(VIABLES_SEEDS ~ SELFING, data = SF_2019)
 k <- lm(VIABLES_SEEDS ~ SELFING, data = SF_2019)
 summary(k)
+#I would not test all species at once, and if so, use random factors. 
+#I would check model assumptions using plot(k) 
+#and looking at the residuals distribution
 
 SF_2019$SELFING <- as.factor(SF_2019$SELFING)
 str(SF_2019)
 SF_2019$VIABLES_SEEDS <- as.numeric(SF_2019$VIABLES_SEEDS)
-par(mfrow=c(1,1))
+#I am concerned this is needed. It should be read as numeric directly. 
+#Make sure you are not droping any non numeric value that was intended to be numeric. e.g. 1_2
+par(mfrow=c(1,1)) 
 
 SF <- ggplot(SF_2019, aes(x= SELFING, y = VIABLES_SEEDS))+
     geom_point(aes(color=PLANT))+
@@ -24,13 +37,14 @@ SF #hacerlo con cajas estaría mejor
 #BUscar soluciones, no me sale este barplot
 #PLANT<-factor(SF_2019$PLANT, levels = c ("CHFU","LEMA"))
 #barplot(table(SF_2019$VIABLES_SEEDS, PLANT), beside = T, legend.text = T, col= c(123, 345))
-
+#IB: Con los datos me lo miro. 
 
 #chfu----
 SF_CHFU <- subset(SF_2019, PLANT== 'CHFU')
 boxplot(SF_CHFU$VIABLES_SEEDS~SF_CHFU$SELFING, xlab= ('selfing'), ylab = ('viable seeds'), main = ('Viable seeds vs selfing: CHFU'))
 m <- lm(VIABLES_SEEDS ~ SELFING, data = SF_CHFU)
 summary(m)
+plot(m) #check residuals
 aov(VIABLES_SEEDS ~ SELFING, data = SF_CHFU)
 
 #LEMA ----
@@ -50,28 +64,5 @@ plot(O)
 
 #según los analisis anteriores se podría decir que a CHFU los polinizadores le producen un mayor fitness 
     #que a LEMA y PUPA, que no dependen tanto de los polinizadores. LEMa tiene altas tasas de autofecundación
-################
-# seed set --> selfing 
-library(tidyverse)
-boxplot(SF_2019$VIABLES_SEEDS~ SF_2019$SELFING, xlab= ('selfing'), col= c(123, 456), ylab = ('viable seeds'),main = ('Viable seeds vs selfing: LEMA, PUPA y CHFU'))
-t.test(SF_2019$VIABLES_SEEDS~SF_2019$SELFING, data= SF_2019)
-SF_2019$SELFING <- as.factor(SF_2019$SELFING)
-str(SF_2019)
-SF_2019$VIABLES_SEEDS <- as.numeric(SF_2019$VIABLES_SEEDS)
-par(mfrow=c(1,1))
-
-SF <- ggplot(SF_2019, aes(x= SELFING, y = VIABLES_SEEDS))+
-    geom_point(aes(color=PLANT))+
-    xlab('SELFING')+
-    ylab('SEMILLAS VIABLES')+
-    ggtitle ('SEMILLAS VIABLES')+
-NULL
-SF #estaría mejor si consigo hacerlo con cajas, 2 por especie (1 de no viables y otra de viables)
-
-#BUscar soluciones, no me sale este barplot
-#PLANT= factor(SF_2019$PLANT, levels = c ("CHFU","LEMA"))
-#barplot(table(SF_2029$VIABLES_SEEDS, PLANT), beside = TRUE, legend.text = T, col= c(123, 345))
-
-
-
+#Gran primer resultado!
 
