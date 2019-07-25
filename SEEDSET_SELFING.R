ea9699fc1210761a6cc0522b2e29b2bd8f61aa8b
María HMR <maria.hurtado.m.r@gmail.com>
2019-07-25 11:33:10 +0200

    Corrections and some doubts -> RESULTS

--- SEEDSET_SELFING.R
+++ SEEDSET_SELFING.R
@@ -1,60 +1,41 @@
 # seed set --> selfing 
 
 <<<<<<< HEAD
-SF_2019 <- read.table("SF_19.csv", header= T, sep= ";")
-
-
-library(tidyverse)
-
-boxplot(SF_2019$VIABLES_SEEDS~ SF_2019$SELFING, xlab= ('selfing'), col= c(123, 456,234), ylab = ('viable seeds'),main = ('Viable seeds vs selfing: LEMA, PUPA Y CHFU'))
-=======
 #load libraries:
 library(tidyverse)
 
-#load data:
-#Please load it here, or is hard to guess which file you are using.
-# In fact, there is no data in the repo!! Add it as csv's.
-# It means I am reading over the code, but without actually beeing able to run it.
-SF_2019 <- read.csv("data/SF_19.csv", sep = ";")
+#load data
+SF_2019 <- read.table("SF_19.csv", header= T, sep= ";")
 head(SF_2019)
 
-#SF_2019 es el archivo de selfing de pupa, lema y chfu
-boxplot(SF_2019$VIABLES_SEEDS ~ SF_2019$SELFING, xlab= ('selfing'), col= c(123, 456,234), ylab = ('viable seeds'),main = ('Viable seeds vs selfing: LEMA, PUPA Y CHFU'))
->>>>>>> 155c47957f48989c86557b9b4954d5a2887a5d21
-t.test(SF_2019$VIABLES_SEEDS~SF_2019$SELFING, data= SF_2019)
 
-aov(VIABLES_SEEDS ~ SELFING, data = SF_2019)
-k <- lm(VIABLES_SEEDS ~ SELFING, data = SF_2019)
-summary(k)
-plot(k)
+#Analysis
+#boxplot(SF_2019$VIABLES_SEEDS~ SF_2019$SELFING, xlab= ('selfing'), col= c(123, 456,234), ylab = ('viable seeds'),main = ('Viable seeds vs selfing: LEMA, PUPA Y CHFU'))
+
+#boxplot(SF_2019$VIABLES_SEEDS ~ SF_2019$SELFING, xlab= ('selfing'), col= c(123, 456,234), ylab = ('viable seeds'),main = ('Viable seeds vs selfing: LEMA, PUPA Y CHFU'))
+#t.test(SF_2019$VIABLES_SEEDS~SF_2019$SELFING, data= SF_2019)
+
+#aov(VIABLES_SEEDS ~ SELFING, data = SF_2019)
+#k <- lm(VIABLES_SEEDS ~ SELFING, data = SF_2019)
+#summary(k)
+#plot(k)
 #I would not test all species at once, and if so, use random factors. 
 #I would check model assumptions using plot(k) 
 #and looking at the residuals distribution.
 #I am ignoring this joint test for now.
 
-SF_2019$SELFING <- as.factor(SF_2019$SELFING)
-str(SF_2019)
-<<<<<<< HEAD
-SF_2019$VIABLES_SEEDS <- as.numeric(SF_2019$VIABLES_SEEDS)
-=======
-SF_2019$VIABLES_SEEDS <- as.numeric(SF_2019$VIABLES_SEEDS) #alreadi integer in my dataset, so this is not needed.
+#SF_2019$SELFING <- as.factor(SF_2019$SELFING)
+#str(SF_2019)
+#SF_2019$VIABLES_SEEDS <- as.numeric(SF_2019$VIABLES_SEEDS)
+#SF_2019$VIABLES_SEEDS <- as.numeric(SF_2019$VIABLES_SEEDS) #alreadi integer in my dataset, so this is not needed.
 
-par(mfrow=c(1,1)) 
-
-SF <- ggplot(SF_2019, aes(x= SELFING, y = VIABLES_SEEDS))+
-    geom_point(aes(color=PLANT))+
-    xlab('SELFING')+
-    ylab('SEMILLAS VIABLES')+
-    ggtitle ('SEMILLAS VIABLES')+
-NULL
-SF #hacerlo con cajas estaría mejor
 
 
 #Buscar soluciones, no me sale este barplot
 #PLANT<-factor(SF_2019$PLANT, levels = c ("CHFU","LEMA"))
 #barplot(table(SF_2019$VIABLES_SEEDS, PLANT), beside = T, legend.text = T, col= c(123, 345))
 #IB: Con los datos me lo miro. 
->>>>>>> 155c47957f48989c86557b9b4954d5a2887a5d21
+
 
 #chfu----
 SF_CHFU <- subset(SF_2019, PLANT== 'CHFU')
@@ -62,16 +43,14 @@ boxplot(SF_CHFU$VIABLES_SEEDS~SF_CHFU$SELFING, xlab= ('selfing'), ylab = ('viabl
 m <- lm(VIABLES_SEEDS ~ SELFING, data = SF_CHFU)
 summary(m)
 plot(m) #check residuals. The model is not perfect, but is good enough. 
-aov(VIABLES_SEEDS ~ SELFING, data = SF_CHFU) #I wuold report in text the summary results only.
+#aov(VIABLES_SEEDS ~ SELFING, data = SF_CHFU) #I wuold report in text the summary results only.
 
-#otra manera
+#alternative analysis
 SF_CHFU$TOTAL_SEEDS <- as.numeric(as.character(SF_CHFU$TOTAL_SEEDS))
 boxplot((SF_CHFU$VIABLES_SEEDS/SF_CHFU$TOTAL_SEEDS)~SF_CHFU$SELFING, xlab= ('selfing'), ylab = ('% of viable seeds'), main = ('Viable seeds vs selfing: CHFU'), las = 1, col=c("blue4", "chartreuse"))
 n3 <- glm(cbind(SF_CHFU$VIABLES_SEEDS,SF_CHFU$NO_VIABLES_SEEDS) ~ SF_CHFU$SELFING, family = "binomial") #no funciona
 summary(n3)
 
-#El primer boxplot(SF_CHFU$VIABLES_SEEDS~SF_CHFU$SELFING) sale significiativo, mientras que el segundo no (SF_CHFU$VIABLES_SEEDS/SF_CHFU$TOTAL_SEEDS)~SF_CHFU$SELFING). 
-#Esto puede ser porque el selfing en CHFU parece que afecta al numero total de semillas, y no tanto en la viabilidad de estas.
 
 #LEMA ----
 SF_LEMA <- subset(SF_2019, PLANT== 'LEMA')
@@ -79,52 +58,37 @@ boxplot(SF_LEMA$VIABLES_SEEDS~SF_LEMA$SELFING, xlab= ('selfing'), ylab = ('viabl
 n <- lm(VIABLES_SEEDS ~ SELFING, data = SF_LEMA)
 summary(n)
 plot(n) #this is completely fine.
-aov(VIABLES_SEEDS ~ SELFING, data = SF_LEMA)
+#aov(VIABLES_SEEDS ~ SELFING, data = SF_LEMA)
 
-<<<<<<< HEAD
-#otra manera
-SF_LEMA$TOTAL_SEEDS <- as.numeric(as.character(SF_LEMA$TOTAL_SEEDS))
-boxplot((SF_LEMA$VIABLES_SEEDS/SF_LEMA$TOTAL_SEEDS)~SF_LEMA$SELFING, xlab= ('selfing'), ylab = ('% of viable seeds'), main = ('Viable seeds vs selfing: LEMA'), las = 1, col=c("blue4", "chartreuse"))
-=======
 #alternative analysis
 SF_LEMA$TOTAL_SEEDS <- as.numeric(as.character(SF_LEMA$TOTAL_SEEDS))
-boxplot((SF_LEMA$VIABLES_SEEDS/SF_LEMA$TOTAL_SEEDS)~SF_LEMA$SELFING, xlab= ('selfing'), ylab = ('% of viable seeds'), main = ('Viable seeds vs selfing: LEMA'), las = 1)
->>>>>>> 155c47957f48989c86557b9b4954d5a2887a5d21
+boxplot((SF_LEMA$VIABLES_SEEDS/SF_LEMA$TOTAL_SEEDS)~SF_LEMA$SELFING, xlab= ('selfing'), ylab = ('% of viable seeds'), main = ('Viable seeds vs selfing: LEMA'), las = 1, col=c("blue4", "chartreuse"))
 n1 <- glm(cbind(SF_LEMA$VIABLES_SEEDS,SF_LEMA$NO_VIABLES_SEEDS) ~ SF_LEMA$SELFING, family = "binomial")
 summary(n1)
+
+
 #SURPRISE, selfing has more viable seeds! This is super cool, because it match our results showing coleopterans damaging seed development.
 #we do not use plot here, because the family is not gaussian. binomial is very robust to model assumptions, but if you want, you can test it with packahe dharma.
-#Can you repeat this analysis for the other two plants?
+
 
 #pupa ----
 SF_PUPA <- subset(SF_2019, PLANT== 'PUPA')
-<<<<<<< HEAD
 boxplot(SF_PUPA$VIABLES_SEEDS~SF_PUPA$SELFING, xlab= ('selfing'), ylab = ('viable seeds'), outline= F, las=1, main = ('Viable seeds vs selfing: PUPA'),col=c("blue4", "chartreuse"))
-aov(VIABLES_SEEDS ~ SELFING, data = SF_PUPA)
+#aov(VIABLES_SEEDS ~ SELFING, data = SF_PUPA)
 O <- lm(VIABLES_SEEDS ~ SELFING, data = SF_PUPA)
 summary(O)
 plot(O)#good model.
-#otra manera
 
+#alternative analysis
 SF_PUPA$TOTAL_SEEDS <- as.numeric(as.character(SF_PUPA$TOTAL_SEEDS))
 boxplot((SF_PUPA$VIABLES_SEEDS/SF_PUPA$TOTAL_SEEDS)~SF_PUPA$SELFING, xlab= ('selfing'), ylab = ('% of viable seeds'), main = ('Viable seeds vs selfing: PUPA'), las = 1, col=c("blue4", "chartreuse"), outline=F)
 n2 <- glm(cbind(SF_PUPA$VIABLES_SEEDS,SF_PUPA$NO_VIABLES_SEEDS) ~ SF_PUPA$SELFING, family = "binomial")
 summary(n2)
 
-=======
-boxplot(SF_PUPA$VIABLES_SEEDS~SF_PUPA$SELFING, xlab= ('selfing'), ylab = ('viable seeds'), main = ('Viable seeds vs selfing: PUPA'))
-# I would double check the 2 outlyers (> 10 seeds). Maybe plot it without them for clarity?
-boxplot(SF_PUPA$VIABLES_SEEDS~SF_PUPA$SELFING, xlab= ('selfing'), ylab = ('viable seeds'), main = ('Viable seeds vs selfing: PUPA'),
-        ylim = c(0,5), las = 1) #las = 1 is also nice.
-aov(VIABLES_SEEDS ~ SELFING, data = SF_PUPA)
-O <- lm(VIABLES_SEEDS ~ SELFING, data = SF_PUPA)
-summary(O)
-plot(O) #good model.
+
 
 #según los analisis anteriores se podría decir que a CHFU los polinizadores le producen un mayor fitness 
     #que a LEMA y PUPA, que no dependen tanto de los polinizadores. LEMa tiene altas tasas de autofecundación
 #Gran primer resultado!
->>>>>>> 155c47957f48989c86557b9b4954d5a2887a5d21
 
-#según los analisis anteriores se podría decir que a CHFU los polinizadores le producen un mayor fitness 
-    #que a LEMA y PUPA, que no dependen tanto de los polinizadores. LEMa tiene altas tasas de autofecundación
+
