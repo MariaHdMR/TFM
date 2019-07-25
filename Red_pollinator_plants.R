5f7ca869dfe3568c31d6ad90d3e558814ace8d2e
María HMR <maria.hurtado.m.r@gmail.com>
2019-07-25 10:18:26 +0200

    New things

--- Red_pollinator_plants.R
+++ Red_pollinator_plants.R
@@ -3,21 +3,33 @@ library(tidyverse)
 library(bipartite)
 library(igraph)
 
+FV_16_19 <-read.table("FV_16_19.csv", header=T, sep=";")
+FV_19 <- subset(FV_16_19, Year == 2019) 
+Abun_19 <-read.table("Abun_19.csv", header=T, sep=";")
+Abun_19$Plot <- Abun_19$plot
+Abun_19$Subplot <- Abun_19$subplot
+Abun_19$Plant_Simple <- Abun_19$Sp.Focal
+ab.19 <-Abun_19 %>% group_by(Plot, Subplot, Plant_Simple) %>% summarise (num.plantas = sum(Plantas))
+FINAL <- dplyr::left_join(FV.19, ab.19)
+FINAL <- FINAL[which(complete.cases(FINAL)),]
+
 FINAL1 <- subset(FINAL,Plant_Simple %in% c("LEMA","CHFU","RAPE","ME", "HOMA","PUPA", "CHMI") & Group %in% c("Beetle", "Fly", "Butterfly","Bee"))
 red.general <- FINAL1[,c("Plant_Simple","Group","num.visits")]
 red.general <- red.general[which(complete.cases(red.general)),]
 red.total <- red.general %>% group_by(Group,Plant_Simple) %>% summarise (total.visitas = sum(num.visits))
 red.col <- tidyr::spread(red.total,key = Group, value = total.visitas)
-red.col[is.na(red.col)] <- 0
+#red.col[is.na(red.col)] <- 0
 nombres <- list(red.col$Plant_Simple, names(red.col[,2:length(red.col)]))
 red.matrix <- as.matrix(red.col[,2:length(red.col)], dimnames = nombres)
 rownames(red.matrix) <- red.col$Plant_Simple
-library(tidyverse)
+
 red.igraph <- graph_from_incidence_matrix(red.matrix, weighted = TRUE)
 node.size.df <- FINAL %>% group_by(Plant_Simple) %>% summarise(size = sum(num.plantas))
 plant.size <- node.size.df$size
 names(plant.size) <- node.size.df$Plant_Simple
-visitors.size <- rep(1000,8)
+#visitors.size <- rep(1000,8)
+Abundancia_de_visitors <- dcast(FINAL1, Plant_Simple ~ Group, fun.aggregate = sum, value.var = "num.visits")
+visitors.size <- c(88,450,41,662)*12 #este vector lo he sacado de sumar todas las visitas de Bee, todas las de Beetle...de la matriz anterior
 names(visitors.size) <- nombres[[2]]
 node.size <- c(plant.size,visitors.size)
 
@@ -26,12 +38,12 @@ node.size <- c(plant.size,visitors.size)
 plot(red.igraph, vertex.color=(c("tomato","steelblue")[V(red.igraph)$type+1]),
      # vertex.label=NA,
      # vertex.size=2*igraph::degree(ppIg),
-     vertex.size=node.size/300,
-     edge.width=(edge_attr(red.igraph)$weight)/50, 
+     vertex.size=node.size/200,
+     edge.width=(edge_attr(red.igraph)$weight)/30, 
      edge.color="gray8", 
      rescale = T,
      frame= F,
-     margin= c(0,0,0,0),
+     #margin= c(0,0,0,0),
      asp =2/5,
      edge.label.cex = 40,
      vertex.label.dist= 0,	
@@ -39,3 +51,4 @@ plot(red.igraph, vertex.color=(c("tomato","steelblue")[V(red.igraph)$type+1]),
      vertex.label.color= "gray8",
      #edge.curved=0.3,
      layout=layout_as_bipartite, main="Visitantes florales y  las plantas visitadas")
+
--- spatial_matrix.R
+++ spatial_matrix.R
@@ -1,9 +1,65 @@
 #matriz espacial
-#estos datos solo son del plot 1
+#coordenadas ----
 sitios <- data.frame(name = c("1A","2A","3A","4A","5A","6A","1B","2B", "3B", "4B", "5B","6B","1C","2C","3C","4C","5C","6C","1D","2D","3D","4D","5D","6D","1E","2E","3E","4E", "5E","6E","1F","2F","3F","4F","5F","6F"),lat= c(0.5,0.5,0.5,0.5,0.5,0.5,2,2,2,2,2,2,3.5,3.5,3.5,3.5,3.5,3.5,5,5,5,5,5,5,6.5,6.5,6.5,6.5,6.5,6.5,8,8,8,8,8,8), lng= c(0.5,2,3.5,5,6.5,8,0.5,2,3.5,5,6.5,8,0.5,2,3.5,5,6.5,8,0.5,2,3.5,5,6.5,8,0.5,2,3.5,5,6.5,8,0.5,2,3.5,5,6.5,8))
-h <- dist(sitios, method= "euclidean", diag=T, upper=T)
-summary(h)
+h <- dist(sitios[,c(2,3)], method= "euclidean", diag=T, upper=T)
+# h2 <- dist(sitios, method= "euclidean", diag=T, upper=T)
 
-plot(sitios$lat~sitios$lng)
-plot(h)# ni idea de lo que esta pasando aqui
+summary(h)
+dist.matrix <- as.matrix(h)
+colnames(dist.matrix) <- sitios[,1]
+rownames(dist.matrix) <- sitios[,1]
+
+library(tidyverse)
+tabla.completa.19$Plot <- as.factor(tabla.completa.19$Plot)
+tabla.completa.19$Subplot <- as.factor(tabla.completa.19$Subplot)
+t.19<- tabla.completa.19[which(!is.na(tabla.completa.19$Plot)),] 
+t.19.1 <- t.19[which(!is.na(t.19$Subplot)),]
+
+#para hacerlo con la tabla de visitors abundances
+r.1.b <- subset(V, Plot=='1')
+
+b <- r.1.b%>%group_by(Subplot, )%>% summarise(ab = sum(abun))
+d.visits <- dist(b [,c(2)], method = "euclidean", diag = T, upper = T)
+d.visits.matrix <- as.matrix(d.visits)
+
+
+
+#ahora tendria que hacer lo mismo con las abundancias
+r.1.b <- subset(ab.19, Plot=='1')
+b <- r.1.b%>%group_by(Subplot)%>% summarise(ab = sum(abun))
+d.visits <- dist(b [,c(2)], method = "euclidean", diag = T, upper = T)
+d.visits.matrix <- as.matrix(d.visits)
+
+
+install.packages("ade4")
+library(ade4)
+mantel.rtest(h, d.visits,nrepet = 9999)
+################################################################### Buenos datos----
+#voy a crear una matriz con PUPA y Flies
+#primer paso: crear un subset de los datos de Flies y Pupa para todos los  Plots
+
+abun.P.F <-tabla.P.F %>% group_by(Subplot) %>% summarise(n.abun = sum(abun))
+plantas.P.F <- tabla.P.F %>% group_by(Subplot) %>% summarise(n.plantas = sum(num.plantas))
+Pupa.Flies <-dplyr::left_join(abun.P.F, plantas.P.F )
+
+ab <- dist(abun.P.F, method= "euclidean", diag=T, upper=T)
+summary(ab)
+ab.matrix <- as.matrix(ab)
+#colnames(dist.matrix) <- abun.P.F[,1]
+#☺rownames(dist.matrix) <- abun.P.F[,1]
+
+
+
+plantas1 <- dist(plantas.P.F, method= "euclidean", diag=T, upper=T)
+plantas.matrix <- as.matrix(plantas1)
+mantel.rtest(ab, plantas,nrepet = 9999)
+
+
+#str(Pupa.Flies)
+#geo_data <-Pupa.Flies[,c(3,23:46)] # no me sale, seguir investigando
+geo_data <-Pupa.Flies[,-1]
+#ph_113<-as.geodata(geo_data[1:46,], coords.col = 2:3, data.col = 3)
+
+
+
 
