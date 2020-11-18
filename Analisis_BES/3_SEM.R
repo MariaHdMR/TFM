0c5c7cd66c181e185a5c0673d6e5e65074a81995
María HMR <maria.hurtado.m.r@gmail.com>
2020-11-18 22:00:25 +0100

    SEM

--- Analisis_BES/3_SEM.R
+++ Analisis_BES/3_SEM.R
@@ -4,6 +4,7 @@ library(sem)#SEM
 library(lavaan)#SEM
 library(scales)
 library(tidyverse)
+library(semPlot)
 #>CHFU. ----
 #First I'm going to do the SEM that have the Neighbors intra and inter separate (2 columns) at Plot, 3m,1m and 7.5cm
 #The group argument in the model is the distance level: plot (letter a), 3m (letter c), 1m (letter e), 7.5cm (letter g). 
@@ -29,32 +30,113 @@ Bee ~ n_neighbors_inter
 '
 
 
-modelo.chfu.fl <- ' #This model is for the dataframe neigbors.intra.inter.split
+modelo.chfu.fl <- ' #This model is for the dataframe neigbors.intra.inter.split #◘works with the no contrained
 #regresions
 #Plant_fitness = 
+
 seed ~ Bee
-seed ~ n_neighbors_inter
+seed ~ n_neighbors_inter 
 seed ~ n_neighbors_intra
-Bee ~  n_neighbors_intra
-Bee ~ n_neighbors_inter
 Bee ~ flowers2
-n_neighbors_inter ~~n_neighbors_intra
-n_neighbors_inter ~~ flowers2
-n_neighbors_intra ~~ flowers2
+flowers2 ~ n_neighbors_inter + n_neighbors_intra
+
+seed ~~flowers2
+
 #intercept
 '
 
 
-modelo.chfu.latent <- ' #This model is for the dataframe neigbors.intra.inter.split
+modelo.chfu.1 <- ' #This model is for the dataframe neigbors.intra.inter.split #◘works with the no contrained
+#regresions
+#Plant_fitness = 
+
+seed ~ Bee
+seed ~ inter 
+seed ~ intra
+Bee ~ flowers2
+Bee ~inter
+flowers2 ~ inter + intra
+
+seed ~~flowers2
+
+#intercept
+'
+
+
+
+modelo.chfu.2 <- ' #This model is for the dataframe neigbors.intra.inter.split. works for the no constrained
+#regresions
+#Plant_fitness = 
+seed ~ Bee
+seed ~ n_neighbors_inter 
+Bee ~n_neighbors_inter
+Bee ~n_neighbors_intra
+flowers2 ~ n_neighbors_inter
+flowers2 ~ n_neighbors_intra
+
+seed ~~flowers2
+
+#intercept
+'
+
+
+
+
+
+modelo.chfu.3 <- ' #This model is for the dataframe neigbors.intra.inter.split.  works with the no constrained
+#regresions
+#Plant_fitness = 
+seed ~ Bee
+seed ~ n_neighbors_inter 
+Bee ~n_neighbors_inter
+flowers2 ~ n_neighbors_inter
+flowers2 ~ n_neighbors_intra
+
+seed ~~flowers2
+
+#intercept
+'
+
+modelo.chfu.4 <- ' #This model is for the dataframe neigbors.intra.inter.split.  works with the no constrained
+#regresions
+#Plant_fitness = 
+seed ~ Bee
+seed ~ n_neighbors_inter 
+Bee ~n_neighbors_inter
+flowers2 ~ n_neighbors_inter
+
+seed ~~flowers2
+
+#intercept
+'
+
+
+modelo.chfu.5 <- ' #This model is for the dataframe neigbors.intra.inter.split. 
+#regresions
+#Plant_fitness = 
+seed ~ Bee
+seed ~ n_neighbors_inter 
+Bee ~n_neighbors_inter
+flowers2 ~ n_neighbors_inter
+
+seed ~~flowers2
+seed~seed
+#intercept
+'
+
+
+
+
+modelo.chfu.latent <-  '#This model is for the dataframe neigbors.intra.inter.split
 #regresions
 #Plant_fitness = ~ seed 
 #define the latent variables: V= floral visitors, n= Neighbors
 V = ~ Bee
 N = ~ n_neighbors_inter + n_neighbors_intra 
-
 #structural relations
 seed ~ V+N
-V~~N
+Bee ~n_neighbors_inter
+V~N
 #intercept
 '
 modelo1.chfu <- ' #This model is for the dataframe neigbors.intra.inter
@@ -83,24 +165,138 @@ CHFU.sem1 <- CHFU.sem[,c( "seed", "fruit", "x_coor2", "y_coor2", "Bee","Beetle",
                           "Fly", "flowers2","distance_total","n_neighbors_intra", "n_neighbors_inter")] #there are no visits of 
 #                                        butterflies, so I just need to eliminate it to can do the corregram
 
+
 CHFU.sem1$seed <- rescale(CHFU.sem1$seed)
 CHFU.sem1$n_neighbors_inter <- rescale(CHFU.sem1$n_neighbors_inter)
 CHFU.sem1$n_neighbors_intra <- rescale(CHFU.sem1$n_neighbors_intra)
 CHFU.sem1$flowers2 <- rescale(CHFU.sem1$flowers2)
 CHFU.sem1$Bee <- rescale(CHFU.sem1$Bee)
 
+CHFU.sem1$inter <- CHFU.sem1$n_neighbors_inter
+CHFU.sem1$intra <- CHFU.sem1$n_neighbors_intra
+
 corrgram(CHFU.sem1, order=TRUE, lower.panel=panel.conf, upper.panel=panel.pts, text.panel=panel.txt,
          diag.panel=panel.density, pch=16, lty=1, main="CHFU correlations") #to check the correlation between the variables
-multigroup.8 <- sem(modelo.chfu.fl, CHFU.sem1, group = "distance_total") 
+multigroup.8 <- sem(modelo.chfu.1, CHFU.sem1, group = "distance_total") #the best model is the model.chfu.1 
 summary(multigroup.8, standardize=T)
 varTable(multigroup.8)
 fitMeasures(multigroup.8, c("cfi","rmsea","srmr", "pvalue")) 
 print(modindices(multigroup.8))
 
-
-#no funciona
-multigroup2.constrained.t <- sem(modelo.chfu, CHFU.sem1, group = "distance_total", group.equal = c("intercepts", "regressions"))
-summary(multigroup2.constrained.t)
-anova(multigroup.1, multigroup2.constrained.t)#better the constrained
 par(mfrow=c(1,1))
-semPaths(multigroup2.constrained.t)
+semPaths(multigroup.8)
+
+
+
+
+#LEMA----
+
+modelo.lema.1 <- ' #This model is for the dataframe neigbors.intra.inter.split #works with the no contrained
+#regresions
+#Plant_fitness = 
+seed ~ Beetle
+seed ~ inter 
+seed ~ intra
+Beetle ~flowers2
+Fly ~intra
+flowers2 ~inter+intra
+seed ~~ flowers2
+Beetle ~~ Fly
+Beetle  ~  inter
+#intercept
+'
+
+LEMA.sem <- subset(data, Plant== "LEMA")
+LEMA.sem1 <- LEMA.sem[,c( "seed", "fruit", "x_coor2", "y_coor2", "Butterfly","Beetle",
+                          "Fly", "flowers2","distance_total","n_neighbors_intra", "n_neighbors_inter")] #there are no visits of 
+#                                        butterflies, so I just need to eliminate it to can do the corregram
+
+
+LEMA.sem1$seed <- rescale(LEMA.sem1$seed)
+LEMA.sem1$n_neighbors_inter <- rescale(LEMA.sem1$n_neighbors_inter)
+LEMA.sem1$n_neighbors_intra <- rescale(LEMA.sem1$n_neighbors_intra)
+LEMA.sem1$flowers2 <- rescale(LEMA.sem1$flowers2)
+LEMA.sem1$Butterfly <- rescale(LEMA.sem1$Butterfly)
+
+LEMA.sem1$inter <- LEMA.sem1$n_neighbors_inter
+LEMA.sem1$intra <- LEMA.sem1$n_neighbors_intra
+
+corrgram(LEMA.sem1, order=TRUE, lower.panel=panel.conf, upper.panel=panel.pts, text.panel=panel.txt,
+         diag.panel=panel.density, pch=16, lty=1, main="CHFU correlations") #to check the correlation between the variables
+multigroup.lema <- sem(modelo.lema.1, LEMA.sem1, group = "distance_total") #the best model is the model.chfu.1 
+summary(multigroup.lema, standardize=T)
+varTable(multigroup.lema)
+fitMeasures(multigroup.lema, c("cfi","rmsea","srmr", "pvalue")) #fitea
+print(modindices(multigroup.lema))
+
+par(mfrow=c(1,1))
+semPaths(multigroup.lema)
+
+
+#PUPA ----
+modelo.pupa.1 <- ' #This model is for the dataframe neigbors.intra.inter.split #works with the no contrained
+#regresions
+#Plant_fitness = 
+seed ~ Beetle
+seed ~ inter 
+seed ~ intra
+Beetle ~flowers2
+Fly ~intra
+flowers2 ~inter+intra
+seed ~~ flowers2
+Beetle ~~ Fly
+Beetle  ~  inter
+#intercept
+'
+
+modelo.pupa.2 <- ' #This model is for the dataframe neigbors.intra.inter.split #wnot bad, but can be good
+#regresions
+#Plant_fitness = 
+seed ~ Bee
+seed ~Fly
+seed ~ inter 
+seed ~ intra
+Bee ~inter + flowers2
+Fly ~intra
+
+#intercept
+'
+
+modelo.pupa.3 <- ' #This model is for the dataframe neigbors.intra.inter.split #wnot bad, but can be good
+#regresions
+#Plant_fitness = 
+seed ~ Bee
+seed ~ inter 
+seed ~ intra
+Bee ~inter + flowers2 + intra
+Fly ~intra 
+
+#intercept
+'
+
+PUPA.sem <- subset(data, Plant== "PUPA")
+PUPA.sem1 <- PUPA.sem[,c( "seed", "fruit", "x_coor2", "y_coor2", "Bee","Beetle",
+                          "Fly", "flowers2","distance_total","n_neighbors_intra", "n_neighbors_inter")] #there are no visits of 
+#                                        butterflies, so I just need to eliminate it to can do the corregram
+
+
+PUPA.sem1$seed <- rescale(PUPA.sem1$seed)
+PUPA.sem1$n_neighbors_inter <- rescale(PUPA.sem1$n_neighbors_inter)
+PUPA.sem1$n_neighbors_intra <- rescale(PUPA.sem1$n_neighbors_intra)
+PUPA.sem1$flowers2 <- rescale(PUPA.sem1$flowers2)
+PUPA.sem1$Butterfly <- rescale(PUPA.sem1$Butterfly)
+
+PUPA.sem1$inter <- PUPA.sem1$n_neighbors_inter
+PUPA.sem1$intra <- PUPA.sem1$n_neighbors_intra
+
+corrgram(PUPA.sem1, order=TRUE, lower.panel=panel.conf, upper.panel=panel.pts, text.panel=panel.txt,
+         diag.panel=panel.density, pch=16, lty=1, main="CHFU correlations") #to check the correlation between the variables
+multigroup.pupa <- sem(modelo.pupa.3, PUPA.sem1, group = "distance_total") #the best model is the model.chfu.1 
+summary(multigroup.pupa, standardize=T)
+varTable(multigroup.pupa)
+fitMeasures(multigroup.pupa, c("cfi","rmsea","srmr", "pvalue")) #fitea
+print(modindices(multigroup.pupa))
+
+par(mfrow=c(1,1))
+semPaths(multigroup.pupa)
+
