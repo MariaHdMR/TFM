fed7f68415e4ab0bbaa0d072a32943496efe08d9
María HMR <maria.hurtado.m.r@gmail.com>
2020-12-29 19:00:36 +0100

    SEM

--- Analisis_BES/3_SEM.R
+++ Analisis_BES/3_SEM.R
@@ -161,7 +161,7 @@ Beetle ~ n_neighbors
 
 
 CHFU.sem <- subset(data, Plant== "CHFU")
-CHFU.sem1 <- CHFU.sem[,c( "seed", "fruit", "x_coor2", "y_coor2", "Bee","Beetle",
+CHFU.sem1 <- CHFU.sem[,c( "seed", "fruit","seed.indv" ,"x_coor2", "y_coor2", "Bee","Beetle",
                           "Fly", "flowers2","distance_total","n_neighbors_intra", "n_neighbors_inter")] #there are no visits of 
 #                                        butterflies, so I just need to eliminate it to can do the corregram
 
@@ -171,6 +171,7 @@ CHFU.sem1$n_neighbors_inter <- rescale(CHFU.sem1$n_neighbors_inter)
 CHFU.sem1$n_neighbors_intra <- rescale(CHFU.sem1$n_neighbors_intra)
 CHFU.sem1$flowers2 <- rescale(CHFU.sem1$flowers2)
 CHFU.sem1$Bee <- rescale(CHFU.sem1$Bee)
+CHFU.sem1$seed.indv <- rescale (CHFU.sem1$seed.indv)
 
 CHFU.sem1$inter <- CHFU.sem1$n_neighbors_inter
 CHFU.sem1$intra <- CHFU.sem1$n_neighbors_intra
@@ -183,10 +184,36 @@ varTable(multigroup.8)
 fitMeasures(multigroup.8, c("cfi","rmsea","srmr", "pvalue")) 
 print(modindices(multigroup.8))
 
+
 par(mfrow=c(1,1))
 semPaths(multigroup.8)
 #IB: Mola, podrias añadir en power point o similar los coeficientes de los paths, 
 # y esto sera tu resultado principal de la charla de la BES.
+
+
+#total seed per individual
+modelo.chfu.1.indv <- ' #This model is for the dataframe neigbors.intra.inter.split #◘works with the no contrained
+#regresions
+#Plant_fitness = 
+
+seed.indv ~ Bee
+seed.indv ~ inter 
+seed.indv ~ intra
+Bee ~ flowers2
+Bee ~inter
+flowers2 ~ inter + intra
+
+seed.indv ~~flowers2
+
+#intercept
+'
+multigroup.8.indv <- sem(modelo.chfu.1.indv, CHFU.sem1, group = "distance_total") #the best model is the model.chfu.1 
+summary(multigroup.8.indv, standardize=T)
+varTable(multigroup.8.indv)
+fitMeasures(multigroup.8.indv, c("cfi","rmsea","srmr", "pvalue")) #fittea
+print(modindices(multigroup.8.indv))
+
+
 
 
 
@@ -208,7 +235,7 @@ Beetle  ~  inter
 '
 
 LEMA.sem <- subset(data, Plant== "LEMA")
-LEMA.sem1 <- LEMA.sem[,c( "seed", "fruit", "x_coor2", "y_coor2", "Butterfly","Beetle",
+LEMA.sem1 <- LEMA.sem[,c( "seed", "fruit","seed.indv", "x_coor2", "y_coor2", "Butterfly","Beetle",
                           "Fly", "flowers2","distance_total","n_neighbors_intra", "n_neighbors_inter")] #there are no visits of 
 #                                        butterflies, so I just need to eliminate it to can do the corregram
 
@@ -218,6 +245,13 @@ LEMA.sem1$n_neighbors_inter <- rescale(LEMA.sem1$n_neighbors_inter)
 LEMA.sem1$n_neighbors_intra <- rescale(LEMA.sem1$n_neighbors_intra)
 LEMA.sem1$flowers2 <- rescale(LEMA.sem1$flowers2)
 LEMA.sem1$Butterfly <- rescale(LEMA.sem1$Butterfly)
+LEMA.sem1$seed.indv <- rescale(LEMA.sem1$seed.indv)
+LEMA.sem1$flowers2 <- rescale(LEMA.sem1$flowers2)
+LEMA.sem1$Beetle <- rescale(LEMA.sem1$Beetle)
+LEMA.sem1$Fly <- rescale(LEMA.sem1$Fly)
+
+
+
 
 LEMA.sem1$inter <- LEMA.sem1$n_neighbors_inter
 LEMA.sem1$intra <- LEMA.sem1$n_neighbors_intra
@@ -232,6 +266,37 @@ print(modindices(multigroup.lema))
 
 par(mfrow=c(1,1))
 semPaths(multigroup.lema)
+
+
+
+
+#seed per indv
+modelo.lema.1.indv <- ' #This model is for the dataframe neigbors.intra.inter.split #works with the no contrained
+#regresions
+#Plant_fitness = 
+seed.indv ~ Beetle
+seed.indv ~ inter 
+seed.indv ~ intra
+Beetle ~flowers2
+Fly ~intra
+flowers2 ~inter+intra
+seed.indv ~~ flowers2
+Beetle ~~ Fly
+Beetle  ~  inter
+#intercept
+'
+
+multigroup.lema.indv <- sem(modelo.lema.1.indv, LEMA.sem1, group = "distance_total") #the best model is the model.chfu.1 
+summary(multigroup.lema.indv, standardize=T)
+varTable(multigroup.lema.indv)
+fitMeasures(multigroup.lema.indv, c("cfi","rmsea","srmr", "pvalue")) #fitea
+print(modindices(multigroup.lema.indv))
+
+par(mfrow=c(1,1))
+semPaths(multigroup.lema.indv)
+
+
+
 
 
 #PUPA ----
@@ -277,7 +342,7 @@ Fly ~intra
 
 PUPA.sem <- subset(data, Plant== "PUPA")
 PUPA.sem1 <- PUPA.sem[,c( "seed", "fruit", "x_coor2", "y_coor2", "Bee","Beetle",
-                          "Fly", "flowers2","distance_total","n_neighbors_intra", "n_neighbors_inter")] #there are no visits of 
+                          "Fly", "flowers2","seed.indv", "distance_total","n_neighbors_intra", "n_neighbors_inter")] #there are no visits of 
 #                                        butterflies, so I just need to eliminate it to can do the corregram
 
 
@@ -286,6 +351,7 @@ PUPA.sem1$n_neighbors_inter <- rescale(PUPA.sem1$n_neighbors_inter)
 PUPA.sem1$n_neighbors_intra <- rescale(PUPA.sem1$n_neighbors_intra)
 PUPA.sem1$flowers2 <- rescale(PUPA.sem1$flowers2)
 PUPA.sem1$Butterfly <- rescale(PUPA.sem1$Butterfly)
+PUPA.sem1$seed.indv <- rescale(PUPA.sem1$seed.indv)
 
 PUPA.sem1$inter <- PUPA.sem1$n_neighbors_inter
 PUPA.sem1$intra <- PUPA.sem1$n_neighbors_intra
@@ -300,4 +366,23 @@ print(modindices(multigroup.pupa))
 
 par(mfrow=c(1,1))
 semPaths(multigroup.pupa)
+
+#seed per individuals
+modelo.pupa.3.indv <- ' #This model is for the dataframe neigbors.intra.inter.split #wnot bad, but can be good
+#regresions
+#Plant_fitness = 
+seed.indv ~ Bee
+seed.indv ~ inter 
+seed.indv ~ intra
+Bee ~inter + flowers2 + intra
+Fly ~intra 
+
+#intercept
+'
+
+multigroup.pupa.indv <- sem(modelo.pupa.3.indv, PUPA.sem1, group = "distance_total") #the best model is the model.chfu.1 
+summary(multigroup.pupa.indv, standardize=T)
+varTable(multigroup.pupa.indv)
+fitMeasures(multigroup.pupa.indv, c("cfi","rmsea","srmr", "pvalue")) #fitea
+print(modindices(multigroup.pupa.indv))
 
